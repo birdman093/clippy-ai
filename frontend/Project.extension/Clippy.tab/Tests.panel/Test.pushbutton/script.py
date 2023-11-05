fb9e1f42cfd7f18ed1699467eb286fd7aae3d9ea
Francisco Maranchello <franmaranchello@gmail.com>
2023-11-05 06:18:42 +0000

    Fixed counter and context.

--- frontend/Project.extension/Clippy.tab/Tests.panel/Test.pushbutton/script.py
+++ frontend/Project.extension/Clippy.tab/Tests.panel/Test.pushbutton/script.py
@@ -1,47 +1,40 @@
 import clr
-from System import Array, Byte
-from System.Net import WebClient, WebRequest, WebException
+from System import String
+from System.Net import WebClient, WebRequest
 from System.Text import Encoding
 from System.IO import StreamReader
 from System.Threading import Thread
-from helper import clean_code_snippet, ContextData
+from helper import clean_code_snippet
+from System.Net import WebException
 
-# Add references to the .NET assemblies needed
 clr.AddReference('System')
 clr.AddReference('System.Net')
 clr.AddReference('System.Threading')
 
-
-# Instantiate ContextData
-context_data = ContextData()
-
 input_string = 'From the selected elements, list their lengths by family name'
 
-# Function to clean the response string
 def clean_response_string(response):
-    allowed_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "
+    allowed_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789- "
     cleaned_string = ''.join(c for c in response if c in allowed_chars)
     return cleaned_string
 
-# Main function to send data to the server and handle the response
-def main(input_string, context_data):
+def main(input_string):
     max_attempts = 10
-    context_data.counter = 1
+    counter = 1
+    context = ""
     url = 'http://127.0.0.1:8080/'
     delay_between_requests = 10000  # Delay in milliseconds
 
-    # Create a WebClient instance
     client = WebClient()
     client.Headers.Add("Content-Type", "application/json; charset=utf-8")
 
-    while context_data.counter <= max_attempts:
-        json_data = '{{"client": "{0}"}}'.format(input_string, context_data.context)
-        bytes_data = Encoding.UTF8.GetBytes(json_data)
+    while counter <= max_attempts:
+        json_data = '{{"client": "{0}. {1}."}}'.format(input_string, context)
+        data = Encoding.UTF8.GetBytes(json_data)
         print("Sending data to server: {}".format(json_data))  # Check the JSON structure
 
         try:
-            # Send the data to the server
-            responseBytes = client.UploadData(url, "POST", bytes_data)
+            responseBytes = client.UploadData(url, "POST", data)
             responseString = Encoding.UTF8.GetString(responseBytes)
             clean_code = clean_code_snippet(responseString)
             print("Code response: ", clean_code)
@@ -49,30 +42,24 @@ def main(input_string, context_data):
             return  # Successful execution, exit the loop
         except WebException as webEx:
             if webEx.Response is not None:
-                with webEx.Response.GetResponseStream() as responseStream:
-                    if responseStream is not None:
-                        reader = StreamReader(responseStream)
-                        errorMessage = reader.ReadToEnd()
-                        response_exception = clean_response_string(errorMessage)
-                        print("Server error response: ", response_exception)
-                        if "400 Bad Request" in response_exception:
-                            print("Client error, will not retry.")
-                            break  # Break out of the loop on client error
-                        reader.Close()
+                responseStream = webEx.Response.GetResponseStream()
+                if responseStream is not None:
+                    reader = StreamReader(responseStream)
+                    errorMessage = reader.ReadToEnd()
+                    response_exception = clean_response_string(errorMessage)
+                    print("Server error response: ", response_exception)
             else:
                 print("WebException without response: ", webEx.Message)
         except Exception as e:
             response_exception = clean_response_string(str(e))
             print("Exception: ", response_exception)
         finally:
-            context_data.context = "Consider this error: {}".format(response_exception)
-            context_data.increment_counter()
-            print("Attempt: {}".format(context_data.counter))
-            if context_data.counter > max_attempts:
+            context = "Consider this error: {}".format(response_exception)
+            counter += 1
+            print("Attempt: {}".format(counter))
+            if counter > max_attempts:
                 print("Maximum attempts reached. Exiting.")
                 break  # Ensure to break out of the loop
             Thread.Sleep(delay_between_requests)  # Sleep before the next request
 
-
-# Call the main function with the input string and context data
-main(input_string, context_data)
+main(input_string)
